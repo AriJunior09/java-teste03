1b7fc9ade4715208647b6804e4ddbb347c5fcf5c
arijunior09 <arioliveirajuniorjose@gmail.com>
2024-03-14 23:21:31 -0300

    update

---
+++ ClasseTeste03.java
@@ -0,0 +1,30 @@
+package pacoteTeste03;
+
+import java.util.Scanner;
+
+public class ClasseTeste03 { // Soma duas idades
+
+    public static void main(String[] args) {
+    
+    	Scanner sc = new Scanner(System.in);
+    	
+    	System.out.print("Digite a idade: ");    	
+    	int idade1 = sc.nextInt();
+
+    	System.out.print("Digite a segunda idade: ");    	
+    	int idade2 = sc.nextInt();
+    	
+    	int total = somar(idade1, idade2);
+    	    	
+    	System.out.print("A soma das idades é: " + total);
+    	// teste
+    	
+    	sc.close();
+    	
+    }
+    
+    private static int somar(int a, int b) {
+    	return a+b;
+    }
+}
+
